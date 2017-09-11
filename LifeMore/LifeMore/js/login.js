1c33e19aed4cf859b22e47a348dbe68ee2417e59
mesquini <mesquini@live.com>
2017-09-11 16:36:35 -0300

    Login e index.

---
+++ LifeMore/LifeMore/js/login.js
@@ -0,0 +1,12 @@
+/*
+Originally found at https://cssdeck.com/labs/login-form-using-html5-and-css3
+
+by: https://cssdeck.com/user/kamalchaneman
+
+
+
+
+*/
+function mudaCor(el) {
+    el.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
